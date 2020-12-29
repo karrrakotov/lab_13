7575b180b575dee545040334559c98285fa99f50
karrrakotov <karrrakotov@mail.ru>
2020-12-29 14:51:49 +0300

    Готово

--- individ_1.py
+++ individ_1.py
@@ -9,49 +9,49 @@
 class Triangle:
 
     def __init__(self, first=1, second=1, third=1, square=1):
-        self.__first = float(first)
-        self.__second = float(second)
-        self.__third = float(third)
-        self.__square = float(square)
+        self.first = float(first)
+        self.second = float(second)
+        self.third = float(third)
+        self.square = float(square)
 
     # Вывод данных на экран
     def __str__(self):
-        return f"{self.__first, self.__second, self.__third, self.__square}"
+        return f"{self.first, self.second, self.third, self.square}"
 
     # Сравнение площадей треугольников
     def __lt__(self, other):
-        return self.__square < other.__square
+        return self.square < other.square
 
     def __gt__(self, other):
-        return self.__square > other.__square
+        return self.square > other.square
 
     def __le__(self, other):
-        return self.__square <= other.__square
+        return self.square <= other.square
 
     def __ge__(self, other):
-        return self.__square >= other.__square
+        return self.square >= other.square
 
     def __eq__(self, other):
-        return self.__square == other.__square
+        return self.square == other.square
 
     def __ne__(self, other):
-        return self.__square != other.__square
+        return self.square != other.square
 
     # Выполнения арефметических операций над площадями
     def __add__(self, other):
-        return self.__square + other.__square
+        return self.square + other.square
 
     def __sub__(self, other):
-        if self.__square >= other.__square:
-            return self.__square - other.__square
+        if self.square >= other.square:
+            return self.square - other.square
         else:
             return ValueError("Отрицательная площадь! -_-")
 
     def __mul__(self, other):
-        return self.__square * other.__square
+        return self.square * other.square
 
     def __truediv__(self, other):
-        return self.__square / other.__square
+        return self.square / other.square
 
 
 if __name__ == '__main__':
